c7aa1c43176375d240b4bd599f66258e286b9330
J Ridgeway <sw333zy@gmail.com>
2016-11-02 09:02:05 -0400

    lost 808 connection some sass and normalize problems works but on my screen but very red

--- js/data.js
+++ js/data.js
@@ -1,7 +1,29 @@
 (function() {
-  'use strict';
-  window.ajax = window.ajax || {};
+    'use strict';
+    window.ajax = window.ajax || {};
+    console.log('im here');
 
-  console.log('im here');
+    var myToken;
+
+    $('.tokenButton').on('click', function submitToken(event) {
+        myToken = $('input').val();
+        $.ajax({
+                url: 'https://api.github.com/users/jisaacks/orgs',
+                method: 'GET',
+                dataType: 'json',
+                headers: {
+                    Authorization: 'token ' + myToken
+                }
+
+            })
+            .done(function ajaxSucceed(data) {
+                console.log(data);
+                window.ajax.buildList(data)
+
+            })
+            .fail(function ajaxFailed(xhr) {
+                console.log(xhr);
+            })
+    })
 
 }());
--- js/ui.js
+++ js/ui.js
@@ -1,7 +1,17 @@
 (function() {
-  'use strict';
-  window.ajax = window.ajax || {};
+    'use strict';
+    window.ajax = window.ajax || {};
 
-  console.log('im here');
+    console.log('im here');
+
+    window.ajax.buildList = function buildList(data) {
+        $(data).each(function grabData(i) {
+            $('ul').append('<li><img src="' + data[i].avatar_url + '"/><p>' + data[i].login + '</p></li>');
+        })
+
+        console.log('building list', data)
+
+
+    };
 
 }());
